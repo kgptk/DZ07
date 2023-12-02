a86f323b3b394f3a04147b1a8d2b78c20c8a11ad
kgptk <142275980+kgptk@users.noreply.github.com>
2023-12-02 21:12:29 +0900

    hw 09

---
+++ hw09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+namespace hw09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Anna", 75);
+
+            string jsonPerson = JsonSerializer.Serialize(person);
+            Console.WriteLine(jsonPerson);
+             
+            // json строку десериализуем в объект класса Person
+            Person? restoredPerson = JsonSerializer.Deserialize<Person>(jsonPerson);
+            Console.WriteLine(restoredPerson?.Name);
+
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            // сериализуем объект класса Person в xml
+            using (FileStream fs = new FileStream("p.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, person);
+
+                Console.WriteLine("Object has been serialized");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; } = "Undefined";
+        public int Age { get; set; } = 1;
+
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Age = {Age}";
+        }
+
+    }
+}
+
